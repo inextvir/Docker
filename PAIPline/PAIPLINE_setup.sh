06db27e1b8f734f9ec66fc5840845f2aa5484bfd
kaoutarDaoudHiri <62071207+kaoutarDaoudHiri@users.noreply.github.com>
2020-11-09 03:15:40 +0100

    Update PAIPLINE_setup.sh

--- PAIPline/PAIPLINE_setup.sh
+++ PAIPline/PAIPLINE_setup.sh
@@ -3,11 +3,42 @@
 #	PAIPLINE_setup.sh
 #
 
-
-cd /usr/local/bin
-
+export DEBIAN_FRONTEND=noninteractive
+chmod 777 /root
 apt-get update
 apt-get install -y git python
+#
+##
+###
+####
+#####
+######
+####### Conda available dependencies : bowtie2, Blast
+######
+#####
+####
+###
+##
+#
+conda install -y -c bioconda bowtie2
+conda install -y -c bioconda/label/cf201901 blast
+
+
+#
+##
+###
+####
+#####
+######
+####### install PAIPline
+######
+#####
+####
+###
+##
+#
+
+cd /usr/local/bin
 git clone https://gitlab.com/andreas.andrusch/paipline.git
 git clone https://gitlab.com/andreas.andrusch/database-updater.git
 cd ~
